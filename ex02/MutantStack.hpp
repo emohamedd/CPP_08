04ef3ea41d62d9ebd67e5685d071acbf535cde5f
emohamedd <a.elgaouej@gmail.com>
2023-12-22 17:32:05 +0100

    creating the iterators for the poor stack

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: emohamed <emohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 14:45:15 by emohamed          #+#    #+#             */
-/*   Updated: 2023/12/22 17:13:50 by emohamed         ###   ########.fr       */
+/*   Updated: 2023/12/22 17:29:43 by emohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,35 @@
 
 template<typename T>
 class MutantStack : public std::stack<T> {
+   public:
+    class iterator {
     public:
-        MutantStack() {}
+        iterator(typename std::stack<T>::container_type::iterator it) : it(it) {}
+        T& operator*(){
+            return *it; 
+        }
+        iterator& operator++(){
+            ++it; 
+            return *this; 
+        }
+        iterator& operator--(){
+            --it; 
+            return *this; 
+        }
+        bool operator!=(const iterator& other) const{
+            if (it != other.it)
+                return true;
+            return false;
+        }
+
+    private:
+        typename std::stack<T>::container_type::iterator it;
+    };
+
+    iterator begin(){
+         return iterator(this->c.begin()); 
+    }
+    iterator end(){
+        return iterator(this->c.end());  
+    }
 };
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,22 +6,33 @@
 /*   By: emohamed <emohamed@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/22 17:05:48 by emohamed          #+#    #+#             */
-/*   Updated: 2023/12/22 17:14:13 by emohamed         ###   ########.fr       */
+/*   Updated: 2023/12/22 17:30:56 by emohamed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
 
-int main(){
-
+int main()
+{
     MutantStack<int> mstack;
-
     mstack.push(5);
     mstack.push(17);
-
     std::cout << mstack.top() << std::endl;
-
     mstack.pop();
-
     std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << "stack elements : "<< *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    return 0;
 }
